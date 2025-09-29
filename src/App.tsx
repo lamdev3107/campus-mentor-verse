93174e941606c795880b1573174a0526f8646dc9
Lam Tran <lamtx.3107@gmail.com>
2025-09-29 15:21:16 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Landing from "./pages/Landing";
 import StudyMaterials from "./pages/StudyMaterials";
 import ExamPrep from "./pages/ExamPrep";
 import NotFound from "./pages/NotFound";
@@ -18,8 +17,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Index />} />
+          <Route path="/" element={<Index />} />
           <Route path="/study-materials" element={<StudyMaterials />} />
           <Route path="/exam-prep" element={<ExamPrep />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
